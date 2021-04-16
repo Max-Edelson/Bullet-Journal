2cf877fb013624656de92012c01c84f9700e93aa
Ovijavi <galvanjavier540@gmail.com>
2021-04-16 14:39:40 -0700

    started image class

---
+++ source/item/Image.java
@@ -0,0 +1,7 @@
+public class Image extends Item{
+    //imageFile field:  how are we going to upload files?
+    final int MAX_WIDTH = 100;  //width of journal?
+    final int MAX_HEIGHT = 100; //height of journal?
+
+    
+}
