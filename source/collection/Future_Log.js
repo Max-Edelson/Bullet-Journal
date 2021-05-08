608c87c815dcea7ea033fb04b6e1bb0de2ac65e0
Max-Edelson <meedelson@gmail.com>
2021-05-07 21:04:24 -0700

    testing action

--- source/collection/Future_Log.js
+++ source/collection/Future_Log.js
@@ -1,9 +1,9 @@
 // import the item objects
-// import * from './filewithitemobjects.js'
+//import * from "./filewithitemobjects.js"
 
 export default class Future_Log {
     constructor() {}
-
+	
     currDate = new Date();
     currMonth = currDate.getMonth();
 }
