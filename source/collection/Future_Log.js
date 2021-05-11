52df01d71203abdb0870e243091e28ad39bccc12
Maxim Edelson <56405133+Max-Edelson@users.noreply.github.com>
2021-05-10 17:02:55 -0700

    Update Future_Log.js

--- source/collection/Future_Log.js
+++ source/collection/Future_Log.js
@@ -1,9 +1,9 @@
 //import the item objects
 //import * from "./filewithitemobjects.js"
 
-export default class Future_Log {
+/* export default class Future_Log {
 	constructor() {}
 
 	currDate = new Date();
 	currMonth = currDate.getMonth();
-}
+}*/
