473f016b410cac181c49c56c0a67462290b781ca
sophiaklu <srkluete@ucsd.edu>
2021-06-01 22:23:01 -0700

    FEATURE: Add backend/LocalStorage to future log page

---
+++ source/collection/Entry.js
@@ -0,0 +1,142 @@
+import {Item, Event, Task, Note} from './Item.js';
+class Entry extends HTMLElement {
+
+    /**
+     * @constructor creates a Entry custom element that holds items
+     */
+    constructor(){
+        super();
+
+        const template = document.createElement('template');
+
+        template.innerHTML = `
+        <style>
+            .main-item .sub-item {
+                vertical-align: none;
+                font-size: 20px;
+                padding: 0px;
+                
+            }
+            .sub-item {
+                font-size: 15px;
+            }
+        
+            .sub-section {
+                margin-left:25px;
+                margin-right:-25px;
+            }
+
+            .entry {
+            background: #6a6dcd;
+            box-shadow: 0px 2.92414px 5.84828px rgba(0, 0, 0, 0.12),
+                0px 11.6966px 23.3931px rgba(0, 0, 0, 0.08);
+            border-radius: 5.84828px;
+            height: 20%;
+            padding: 5%;
+            margin: 5%;
+            display: flex;
+            align-items: center;
+            }
+            
+            .entry .main-item .sub-item .sub-section {
+            vertical-align: baseline;
+            float: left;
+            font-size: min(1.75rem, 1.5vw);
+            margin: 0;
+            }
+            
+            .entry button {
+            border: 1px solid rgba(255, 255, 255, 0.75);
+            box-sizing: border-box;
+            border-radius: 2px;
+            vertical-align: auto;
+            float: right;
+            outline: none;
+            margin-left: auto;
+            }
+        </style>
+        <div class="entry">
+            <div class="main-section">
+                <p class="main-item"></p>
+            </div>
+            <div class="sub-section">
+                <p class="sub-item"></p>
+            </div>
+            <button>...</button>
+        </div>
+        `;
+
+        // create a shadow root for this web component
+        this.attachShadow({ mode: 'open' })
+        // attach cloned content of template to shadow DOM 
+        this.shadowRoot.appendChild(template.content.cloneNode(true))
+    }
+
+    /**
+     * getter method for the main item of the entry
+     * @returns item object that is the main item
+     */
+    get mainItem(){
+        return this.getAttribute('main-item');
+    }
+
+    /**
+     * getter method for the sub item of the entry
+     * @returns item object that is the sub item
+     */
+     get subItem(){
+        return this.getAttribute('sub-item');
+    }
+
+    /**
+     * setter method that runs when mainItem attribute is changed
+     * adds all the content of the item to the entry custom element
+     * @param mainItem item object that will server as the main item
+     */
+    set mainItem(mainItem){
+      //  let entryArticle = this.shadowRoot.querySelector('.entry');
+      //  let mainSec = this.shadowRoot.querySelector('.main-section');
+
+        let mainText = this.shadowRoot.querySelector('.main-item');
+        
+        
+        if (mainItem instanceof Event){
+            mainText.textContent = mainItem.title + '/// ' + mainItem.date + ': ' + mainItem.text;
+        }
+
+        else if (mainItem instanceof Task){
+            mainText.textContent = mainItem.text + '/// Deadline: ' + mainItem.deadline;
+        }
+        
+        else{
+            mainText.textContent = mainItem.text;
+        }
+        //console.log(mainItem);
+    }
+
+    /**
+     * setter method that runs when subItem attribute is changed
+     * adds all the content of the item to the entry custom element
+     * @param subItem item object that will server as the sub item
+     */
+     set subItem(subItem){
+        let entryArticle = this.shadowRoot.querySelector('.entry');
+        let subSec = this.shadowRoot.querySelector('.sub-section');
+
+        let subText = this.shadowRoot.querySelector('.sub-item');
+        
+        
+        if (subItem instanceof Event){
+            subText.textContent = subItem.title + '/// ' + subItem.date + ': ' + subItem.text;
+        }
+
+        else if (subItem instanceof Task){
+            subText.textContent = subItem.text + '/// Deadline: ' + subItem.deadline;
+        }
+        else{
+            subText.textContent = subItem.text;
+        }
+    }
+}
+
+customElements.define('journal-entry', Entry);
--- source/frontend/future_log/source.js
+++ source/frontend/future_log/source.js
@@ -1,3 +1,366 @@
+import {Item, Event, Task, Note} from '../../collection/Item.js';
+import LocalStorage from '../../collection/LocalStorage.js';
+
+//getting correct months to display
+var d = new Date();
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+month[12] = "January"; //give more incase we start with Dec
+month[13] = "February";
+month[14] = "March";
+month[15] = "April";
+month[16] = "May";
+var month1 = month[d.getMonth()];
+var month2 = month[d.getMonth()+1];
+var month3 = month[d.getMonth()+2];
+var month4 = month[d.getMonth()+3];
+var month5 = month[d.getMonth()+4];
+var month6 = month[d.getMonth()+5];
+
+// getting the innerHTML of the month headers
+let monthNames = document.getElementsByClassName('month_header');
+// update month headers to correct ones
+for (var i = 0; i < monthNames.length; i++) {
+    monthNames[i].innerHTML = month[d.getMonth()+i];
+}
+
+var currMonth = null;
+
+// get where to add each entry (which month)
+var addEntries = document.getElementsByClassName('add_entries');
+
+/////////
+//MODAL//
+/////////
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("new_item1");
+var btn2 = document.getElementById("new_item2");
+var btn3 = document.getElementById("new_item3");
+var btn4 = document.getElementById("new_item4");
+var btn5 = document.getElementById("new_item5");
+var btn6 = document.getElementById("new_item6");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the new item button, open the modal for input
+btn1.onclick = function() {
+    modal.style.display = "block";
+    currMonth = monthNames[0].innerHTML;
+}
+btn2.onclick = function() {
+    modal.style.display = "block";
+    currMonth = monthNames[1].innerHTML;
+}
+btn3.onclick = function() {
+    modal.style.display = "block";
+    currMonth = monthNames[2].innerHTML;
+}
+btn4.onclick = function() {
+    modal.style.display = "block";
+    currMonth = monthNames[3].innerHTML;
+}
+btn5.onclick = function() {
+    modal.style.display = "block";
+    currMonth = monthNames[4].innerHTML;
+}
+btn6.onclick = function() {
+    modal.style.display = "block";
+    currMonth = monthNames[5].innerHTML;
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+    currMonth = null;
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//////////
+// Form //
+//////////
+
+let storage = new LocalStorage();
+let entries = storage.entries; // get list of entries 
+let custom = storage.custom; // get list of entries 
+
+let form = document.querySelector('#entry-form');
+let note = document.querySelector('#entry-content');
+let subnote = document.querySelector('#subentry-content');
+let subButton = document.querySelector("#subButton");
+let subLabel = document.querySelector('#sublabel');
+let subBreak = document.querySelector('#sub-break');
+let subSection = document.querySelector('#sub-section');
+
+let eventTitle = document.querySelector('#entry-title');
+let eventDate = document.querySelector('#entry-date');
+let subeventTitle = document.querySelector('#subentry-title');
+let subeventDate = document.querySelector('#subentry-date');
+
+let taskDeadline = document.querySelector('#deadline');
+let subtaskDeadline = document.querySelector('#subdeadline');
+
+let eventButton = document.querySelector('button.event');
+let taskButton = document.querySelector('button.task');
+let noteButton = document.querySelector('button.note');
+let subeventButton = document.querySelector('button.subevent');
+let subtaskButton = document.querySelector('button.subtask');
+let subnoteButton = document.querySelector('button.subnote');
+
+let editButton = document.querySelector('#edit-btn');
+let deleteButton = document.querySelector('#dlt-btn');
+
+let customButton = document.querySelector('#customButton');
+// let customLogSelect = document.querySelector('.customLogs');
+// let customDataList = document.querySelector('#customOptions');
+
+let monthlyDes = document.querySelector('#MonthlyDes');
+let customDes = document.querySelector('#CustomDes');
+
+let currMainItem = "note";
+let currSubItem = "note";
+
+// let addToCustom = false;
+
+let date = new Date();
+
+// add entries in storage to the webpage
+entries.forEach((data) => {
+    createEntryFromData(data);
+});
+/*
+custom.forEach((data) => {
+    createEntryFromData(data);
+});
+*/
+// edit button
+// editButton.addEventListener('click', () => {
+//     storage.update(data);
+// });
+
+// //delete button
+// deleteButton.addEventListener('click', () => {
+//     storage.update(data);
+// });
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // create main item for new entry
+    let mainItem;
+    if (currMainItem == "note"){
+        mainItem = new Note(note.value, '');
+    }
+    else if (currMainItem == "event"){
+        mainItem = new Event(note.value, '', eventTitle.value, eventDate.value);
+    }
+    else{
+        mainItem = new Task(note.value, '', taskDeadline.value);
+    }
+    
+    //create new entry element
+    let newEntry = document.createElement('journal-entry');
+    newEntry.setAttribute('dateMade', date.toDateString());
+    newEntry.setAttribute('timeMade', date.toTimeString());
+    newEntry.setAttribute('dateSet', eventDate.value);
+    newEntry.setAttribute('inCustom', false);
+    newEntry.setAttribute('inFuture', true);
+    newEntry.setAttribute('futureMonth', currMonth);
+    newEntry.mainItem = mainItem;
+    
+    // if add subitem was selected, add sub item attribute to new entry
+    let subItem;
+    if(subSection.hidden == false){
+        
+        if (currSubItem == "note"){
+            subItem = new Note(subnote.value, '');
+        }
+        else if (currSubItem == "event"){
+            subItem = new Event(subnote.value, '', subeventTitle.value, subeventDate.value);
+        }
+        else{
+            subItem = new Task(subnote.value, '', subtaskDeadline.value);
+        }
+        
+        newEntry.subItem = subItem;
+    }
+
+    const data = {
+        main: mainItem,
+        sub: subItem,
+        date: newEntry.getAttribute('dateMade'),
+        time: newEntry.getAttribute('timeMade'),
+        dateSet: newEntry.getAttribute('dateSet'),
+        addToCustom: newEntry.getAttribute('inCustom'),
+        addToFuture: newEntry.getAttribute('inFuture'),
+        futureMonth: newEntry.getAttribute('futureMonth')
+    };
+
+    storage.create(data);
+
+    // add new entry to the webpage
+    for (var i = 0; i < monthNames.length; i++) {
+        // if the month matches a month header
+        console.log(newEntry.futureMonth);
+        console.log(monthNames[i].innerHTML);
+        if (data.futureMonth === monthNames[i].innerHTML) {
+            // add new entry to the webpage
+            console.log('added');
+            addEntries[i].appendChild(newEntry);
+        }
+    }
+
+    // change page to default state
+    subButton.hidden = false;
+    subSection.hidden = true;
+    currMainItem = "note";
+    currSubItem = "note";
+    //customLogSelect.hidden = true;
+    setElementsHidden('event-specific', true);
+    setElementsHidden('task-specific', true);
+    setElementsHidden('subevent-specific', true);
+    setElementsHidden('subtask-specific', true);
+});
+
+subButton.addEventListener('click', () => {
+    subSection.hidden = false;
+    subButton.hidden = true;
+});
+
+eventButton.addEventListener('click', () => {
+    //addToMonthly(true);
+    currMainItem = "event";
+    setElementsHidden('event-specific', false);
+    setElementsHidden('task-specific', true);
+})
+
+taskButton.addEventListener('click', () => {
+    //addToMonthly(true);
+    currMainItem = "task";
+    setElementsHidden('event-specific', true);
+    setElementsHidden('task-specific', false);
+})
+
+noteButton.addEventListener('click', () => {
+    //addToMonthly(true);
+    currMainItem = "note";
+    noteButton.hidden = false;
+    setElementsHidden('event-specific', true);
+    setElementsHidden('task-specific', true);
+})
+
+subeventButton.addEventListener('click', () => {
+    //addToMonthly(true);
+    currSubItem = "event";
+    setElementsHidden('subevent-specific', false);
+    setElementsHidden('subtask-specific', true);
+})
+
+subtaskButton.addEventListener('click', () => {
+    //addToMonthly(true);
+    currSubItem = "task";
+    setElementsHidden('subevent-specific', true);
+    setElementsHidden('subtask-specific', false);
+})
+
+subnoteButton.addEventListener('click', () => {
+    //addToMonthly(true);
+    currSubItem = "note";
+    noteButton.hidden = false;
+    setElementsHidden('subevent-specific', true);
+    setElementsHidden('subtask-specific', true);
+})
+
+// customButton.addEventListener('click', () => {
+//     if (customButton.checked == true) {
+//         addToMonthly(false);
+//     }
+//     if (customButton.checked == false) {
+//         addToMonthly(true);
+//     }
+//     currSubItem = "note";
+//     currMainItem = "note";
+//     setElementsHidden('event-specific', true);
+//     setElementsHidden('task-specific', true);
+//     setElementsHidden('subevent-specific', true);
+//     setElementsHidden('subtask-specific', true);
+// });
+
+function setElementsHidden(className, newHiddenVal){
+    let eventElements = document.getElementsByClassName(className);
+    let eventElement;
+    for(eventElement of eventElements){
+        eventElement.hidden = newHiddenVal;
+    }
+};
+/*
+function addToMonthly(newBool){
+    monthlyDes.hidden = !newBool;
+    customDes.hidden = newBool;
+   // customLogSelect.hidden = newBool;
+    addToCustom = !newBool;
+}
+*/
+
+
+
+function createEntryFromData(data){
+    // if data is in the future log
+    if (data.addToFuture === "true") {
+        let newEntry = document.createElement('journal-entry');
+        newEntry.setAttribute('dateMade', data.date);
+        newEntry.setAttribute('timeMade', data.time);
+        newEntry.setAttribute('dateSet', data.dateSet);
+        newEntry.setAttribute('inCustom', data.addToCustom);
+        newEntry.setAttribute('inFuture', data.addToFuture);
+        newEntry.setAttribute('futureMonth', data.futureMonth);
+        newEntry.mainItem = data.main;
+        if (data.sub != undefined){
+            newEntry.subItem = data.sub;
+        }  
+        for (var i = 0; i < monthNames.length; i++) {
+            // if the month matches a month header
+            console.log(newEntry.futureMonth);
+            console.log(monthNames[i].innerHTML);
+            if (data.futureMonth === monthNames[i].innerHTML) {
+                // add new entry to the webpage
+                console.log('added');
+                addEntries[i].appendChild(newEntry);
+            }
+        }
+        
+    }
+    
+
+    // add new entry to the webpage
+    // let main = document.querySelector('main');
+    // main.appendChild(newEntry);
+}
+
+
+///////////////////////////
+// Sidebar functionality //
+///////////////////////////
 $(document).ready(function () {
     $("#sidebar").mCustomScrollbar({
         theme: "minimal"
