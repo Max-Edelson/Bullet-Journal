8f326fc40ee261218b366580ba84dbddc8f6a793
Max-Edelson <meedelson@gmail.com>
2021-06-02 10:10:34 -0700

    Isolate just the linting action in a temporary branch

--- source/testing/Monthly_Log.test.js
+++
@@ -1,8 +0,0 @@
-import module from '../collection/Monthly_Log';
-
-/* test('testing event object', () => {
-    let event = new module.Event('textHolder', 'media holder', 'testing event', '...');
-    event.addDate(new Date(2021, 20, 5));
-    expect(event.date).toEqual(new Date(2021, 20, 5));
-}); */
-
