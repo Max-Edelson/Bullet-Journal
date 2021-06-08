46b8cb85e2962d8c2874019f950bd3878a87951e
Khyat24 <57049440+Khyat24@users.noreply.github.com>
2021-06-07 18:39:27 -0700

    Daily log - 06-07-2021

--- script.js
+++ script.js
@@ -1,3 +1,74 @@
+const Confirm = {
+    open (options) {
+        options = Object.assign({}, {
+            title: '',
+            message: '',
+            okText: 'Confirm',
+            cancelText: 'Cancel',
+            onok: function () {},
+            oncancel: function () {}
+        }, options);
+        
+        const html = `
+            <div class="confirm">
+                <div class="confirm__window">
+                    <div class="confirm__titlebar">
+                        <span class="confirm__title">${options.title}</span>
+                    </div>
+                    <div class="confirm__content">${options.message}</div>
+                    <div class="confirm__buttons">
+                        <button class="confirm__button confirm__button--cancel">${options.cancelText}</button>
+                        <button class="confirm__button confirm__button--ok confirm__button--fill">${options.okText}</button>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        const template = document.createElement('template');
+        template.innerHTML = html;
+
+        // Elements
+        const confirmEl = template.content.querySelector('.confirm');
+        const btnOk = template.content.querySelector('.confirm__button--ok');
+        const btnCancel = template.content.querySelector('.confirm__button--cancel');
+
+        confirmEl.addEventListener('click', e => {
+            if (e.target === confirmEl) {
+                options.oncancel();
+                this._close(confirmEl);
+            }
+        });
+
+        btnOk.addEventListener('click', () => {
+            options.onok();
+            this._close(confirmEl);
+        });
+
+        [btnCancel].forEach(el => {
+            el.addEventListener('click', () => {
+                options.oncancel();
+                this._close(confirmEl);
+            });
+        });
+
+        document.body.appendChild(template.content);
+    },
+
+    _close (confirmEl) {
+        confirmEl.classList.add('confirm--close');
+
+        confirmEl.addEventListener('animationend', () => {
+            document.body.removeChild(confirmEl);
+        });
+    }
+};
+
+
+
+
+
+
+
 function togglePopup1(){
     document.getElementById("popup-1").classList.toggle("active");
 }
@@ -12,7 +83,9 @@ function togglePopup3(){
 let addBtn1 = document.querySelector('.addBtn1');
 showNotes();
 addBtn1.addEventListener('click', function (e) {
-    let addText = document.querySelector('#taskInput');
+    let addText = document.querySelector('#taskInput').value;
+    let description1 = document.querySelector("#description1").value
+    let date1 = document.querySelector("#date1").value
     let notes = localStorage.getItem('notes');
     if (notes == null) {
         notesObj = []
@@ -20,9 +93,11 @@ addBtn1.addEventListener('click', function (e) {
     else {
         notesObj = JSON.parse(notes)
     }
-    notesObj.push(addText.value);
+    notesObj.push([addText,description1,date1]);
     localStorage.setItem('notes', JSON.stringify(notesObj))
     addText.value = '';
+    description1.value = '';
+    date1.value = '';
     console.log(notesObj);
     showNotes();
 })
@@ -37,7 +112,22 @@ function showNotes() {
     }
     let html = '';
     notesObj.forEach(function (element, index) {
-        html +=  `<li class="taskLi">${element} <div class="deleteOption" id='${index}' onclick='deleteNote(this.id)'><i class="fas fa-minus-circle"></i></div></li>`;
+        html +=  `<li class="taskLi">
+                    <div class="liMainWrap">
+                    <span class="dateSpan">${element[2]}</span>
+                    <h3>${element[0]}</h3>
+                    <p>${element[1]}</p>
+                    <ul class="mouseover">
+                    <li>
+                    <span class="ellips"><i class="fas fa-ellipsis-h"></i></span>
+                    <ul>
+                    <div class="deleteOption optionSelect" id='${index}' onclick='deleteNote(this.id)'><span style="padding:0 !important">delete</span><i class="fas fa-trash"></i></div>
+                    <div class="editOption optionSelect" id="editBtn"><span style="padding:0 !important">edit</span><i class="fas fa-pen"></i></div>
+                    </ul>
+                    </li>
+                    </ul>
+                    </div>
+                    </li>`;
     });
     let notesElm = document.getElementById('taskUl');
     if(notesObj.length != 0){
@@ -48,17 +138,24 @@ function showNotes() {
     }
 }
 function deleteNote(index){
-    console.log('this is del', index);
-    let notes = localStorage.getItem('notes');
-    if (notes == null) {
-        notesObj = []
-    }
-    else {
-        notesObj = JSON.parse(notes)
-    }
-    notesObj.splice(index, 1);
-    localStorage.setItem('notes', JSON.stringify(notesObj));
-    showNotes();
+    Confirm.open({
+        title: `<i class="fas fa-info-circle"></i>`,
+        message: `<h3>Are you sure you want to delete this entry?</h3><p>You can't undo this action</p>`,
+        onok: () => {
+            console.log('this is del', index);
+            let notes = localStorage.getItem('notes');
+            if (notes == null) {
+                notesObj = []
+            }
+            else {
+                notesObj = JSON.parse(notes)
+            }
+            notesObj.splice(index, 1);
+            localStorage.setItem('notes', JSON.stringify(notesObj));
+            showNotes();
+        }
+      })
+        
 }
 
 
@@ -67,7 +164,9 @@ function deleteNote(index){
 let addBtn2 = document.querySelector('.addBtn2');
 showTodos();
 addBtn2.addEventListener('click', function (e) {
-    let addText2 = document.querySelector('#eventInput');
+    let addText2 = document.querySelector('#eventInput').value;
+    let description2 = document.querySelector('#description2').value;
+    let date2 = document.querySelector("#date2").value;
     let todos = localStorage.getItem('todos');
     if (todos == null) {
         todosObj = []
@@ -75,9 +174,11 @@ addBtn2.addEventListener('click', function (e) {
     else {
         todosObj = JSON.parse(todos)
     }
-    todosObj.push(addText2.value);
+    todosObj.push([addText2,description2,date2]);
     localStorage.setItem('todos', JSON.stringify(todosObj))
     addText2.value = '';
+    description2.value = '';
+    date2.value = '';
     console.log(todosObj);
     showTodos();
 })
@@ -91,8 +192,23 @@ function showTodos() {
         todosObj = JSON.parse(todos)
     }
     let html2 = '';
-    todosObj.forEach(function (element2, index) {
-        html2 +=  `<li class="eventLi">${element2} <div class="deleteOption" id='${index}' onclick='deleteTodos(this.id)'><i class="fas fa-minus-circle"></i></div></li>`;
+    todosObj.forEach(function (element, index) {
+        html2 +=  `<li class="eventLi">
+                    <div class="liMainWrap">
+                    <span class="dateSpan">${element[2]}</span>
+                    <h3>${element[0]}</h3>
+                    <p>${element[1]}</p>
+                    <ul class="mouseover">
+                    <li>
+                    <span class="ellips"><i class="fas fa-ellipsis-h"></i></span>
+                    <ul>
+                    <div class="deleteOption optionSelect" id='${index}' onclick='deleteTodos(this.id)'><span style="padding:0 !important">delete</span><i class="fas fa-trash"></i></div>
+                    <div class="editOption optionSelect" id="editBtn"><span style="padding:0 !important">edit</span><i class="fas fa-pen"></i></div>
+                    </ul>
+                    </li>
+                    </ul>
+                    </div>
+                    </li>`;
     });
     let todosElm = document.getElementById('eventUl');
     if(todosObj.length != 0){
@@ -103,18 +219,30 @@ function showTodos() {
     }
 }
 function deleteTodos(index){
-    console.log('this is del', index);
-    let todos = localStorage.getItem('todos');
-    if (todos == null) {
-        todosObj = []
-    }
-    else {
-        todosObj = JSON.parse(todos)
-    }
-    todosObj.splice(index, 1);
-    localStorage.setItem('todos', JSON.stringify(todosObj));
-    showTodos();
+    Confirm.open({
+        title: `<i class="fas fa-info-circle"></i>`,
+        message: `<h3>Are you sure you want to delete this entry?</h3><p>You can't undo this action</p>`,
+        onok: () => {
+            console.log('this is del', index);
+            let todos = localStorage.getItem('todos');
+            if (todos == null) {
+                todosObj = []
+            }
+            else {
+                todosObj = JSON.parse(todos)
+            }
+            todosObj.splice(index, 1);
+            localStorage.setItem('todos', JSON.stringify(todosObj));
+            showTodos();
+        }
+      })
+    
 }
+
+
+
+
+
 
 
 
@@ -123,7 +251,8 @@ function deleteTodos(index){
 let addBtn3 = document.querySelector('.addBtn3');
 showNewtodo();
 addBtn3.addEventListener('click', function (e) {
-    let addText3 = document.querySelector('#noteInput');
+    let addText3 = document.querySelector('#noteInput').value;
+    let description3 = document.querySelector('#description3').value;
     let newtodo = localStorage.getItem('newtodo');
     if (newtodo == null) {
         newtodoObj = []
@@ -131,9 +260,10 @@ addBtn3.addEventListener('click', function (e) {
     else {
         newtodoObj = JSON.parse(newtodo)
     }
-    newtodoObj.push(addText3.value);
+    newtodoObj.push([addText3,description3]);
     localStorage.setItem('newtodo', JSON.stringify(newtodoObj))
     addText3.value = '';
+    description3.value = '';
     console.log(newtodoObj);
     showNewtodo();
 })
@@ -147,8 +277,22 @@ function showNewtodo() {
         newtodoObj = JSON.parse(newtodo)
     }
     let html3 = '';
-    newtodoObj.forEach(function (element3, index) {
-        html3 +=   `<li class="noteLi">${element3} <div class="deleteOption noteDelete" id='${index}' onclick='deleteNewtodo(this.id)'><i class="fas fa-minus-circle"></i></div></li>`;
+    newtodoObj.forEach(function (element, index) {
+        html3 +=   `<li class="noteLi">
+                    <div class="liMainWrap">
+                    <h3>${element[0]}</h3>
+                    <p>${element[1]}</p>
+                    <ul class="mouseover">
+                    <li>
+                    <span class="ellips"><i class="fas fa-ellipsis-h"></i></span>
+                    <ul>
+                    <div class="deleteOption optionSelect noteDelete" id='${index}' onclick='deleteNewtodo(this.id)'><span style="padding:0 !important">delete</span><i class="fas fa-trash"></i></div>
+                    <div class="editOption optionSelect" id="editBtn"><span style="padding:0 !important">edit</span><i class="fas fa-pen"></i></div>
+                    </ul>
+                    </li>
+                    </ul>
+                    </div>
+                    </li>`;
     });
     let newtodoElm = document.getElementById('noteUl');
     if(newtodoObj.length != 0){
@@ -159,17 +303,24 @@ function showNewtodo() {
     }
 }
 function deleteNewtodo(index){
-    console.log('this is del', index);
-    let newtodo = localStorage.getItem('newtodo');
-    if (newtodo == null) {
-        newtodoObj = []
-    }
-    else {
-        newtodoObj = JSON.parse(newtodo)
-    }
-    newtodoObj.splice(index, 1);
-    localStorage.setItem('newtodo', JSON.stringify(newtodoObj));
-    showNewtodo();
+    Confirm.open({
+        title: `<i class="fas fa-info-circle"></i>`,
+        message: `<h3>Are you sure you want to delete this entry?</h3><p>You can't undo this action</p>`,
+        onok: () => {
+            console.log('this is del', index);
+            let newtodo = localStorage.getItem('newtodo');
+            if (newtodo == null) {
+                newtodoObj = []
+            }
+            else {
+                newtodoObj = JSON.parse(newtodo)
+            }
+            newtodoObj.splice(index, 1);
+            localStorage.setItem('newtodo', JSON.stringify(newtodoObj));
+            showNewtodo();
+        }
+      })
+    
 }
 
 
