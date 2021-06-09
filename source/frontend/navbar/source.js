901f464dcada287b7b5b3903bae2be3e646539a1
Joshua Hong <jjhong@ucsd.edu>
2021-06-08 17:37:32 -0700

    Add Navbar (One version behind) to  monthly log and daily log Add folder with navbar by itself Update popups with feedback Add new needed popups

---
+++ source/frontend/navbar/source.js
@@ -0,0 +1,22 @@
+$('#sidebar, #content').addClass('active');
+$('#sidebarIcon').addClass('active');
+
+$(document).ready(function () {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $(function(){
+        $('#sidebar, #sidebarIcon').hover(function(){
+            $('#sidebar, #content').removeClass('active');
+            $('#sidebarIcon').removeClass('active');
+            $('.collapse.in').removeClass('in');
+            $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+        },function(){
+            $('#sidebar, #content').addClass('active');
+            $('#sidebarIcon').addClass('active');
+            $('.collapse.in').addClass('in');
+            $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+        }).trigger('mouseleave');
+    });
+});
