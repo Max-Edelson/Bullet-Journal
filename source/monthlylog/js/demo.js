8c2e845a7587275ae88c7d2387b94c3b33f2e563
Joshua Hong <jjhong@ucsd.edu>
2021-06-10 20:15:35 -0700

    Fix duplicate calendar

--- source/monthlylog/js/demo.js
+++ source/monthlylog/js/demo.js
@@ -2,33 +2,33 @@ import {Item, Event, Task, Note} from '../../collection/Item.js';
 import {caleandar} from './caleandar.js';
 import LocalStorage from '../../collection/LocalStorage.js';
 
-// example note to insert
-let mainItem = new Note('Remember to eat', '');
-// example day 
-let date = new Date(2021, 5, 24); 
+// // example note to insert
+// let mainItem = new Note('Remember to eat', '');
+// // example day 
+// let date = new Date(2021, 5, 24); 
 
-//insert note into an entry
-let newEntry = document.createElement('journal-entry');
-newEntry.mainItem = mainItem;
-newEntry.date = date;
+// //insert note into an entry
+// let newEntry = document.createElement('journal-entry');
+// newEntry.mainItem = mainItem;
+// newEntry.date = date;
 
-/**
- * @function createCalendarEntry creates an object to insert into calendar based on context of entry
- * @param entry journal-entry element that holds the item 
- * @returns object that is used by caleandar.js to insert into the calendar
- */
-let createCalendarEntry = function(entry){
-  return {'Date': entry.date, 'Title': entry.main.text};
-};
+// /**
+//  * @function createCalendarEntry creates an object to insert into calendar based on context of entry
+//  * @param entry journal-entry element that holds the item 
+//  * @returns object that is used by caleandar.js to insert into the calendar
+//  */
+// let createCalendarEntry = function(entry){
+//   return {'Date': entry.date, 'Title': entry.main.text};
+// };
 
-var events = [
-  {'Date': new Date(2021, 5, 10), 'Title': 'Doctor appointment at 3:25pm.'}, createCalendarEntry(newEntry)
-];
+// var events = [
+//   {'Date': new Date(2021, 5, 10), 'Title': 'Doctor appointment at 3:25pm.'}, createCalendarEntry(newEntry)
+// ];
 
 
-var settings = {};
-var element = document.getElementById('caleandar');
-caleandar(element, events, settings);
+// var settings = {};
+// var element = document.getElementById('caleandar');
+// caleandar(element, events, settings);
 
 let storage = new LocalStorage(); 
 
